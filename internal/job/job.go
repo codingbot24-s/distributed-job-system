f1db61149813e2a513c5eb137145a6df676c6a89
pcodefromOarch <primecoder8@gmail.com>
2025-11-14 23:37:23 +0530

    add job struct

--- internal/job/job.go
+++ internal/job/job.go
@@ -1,3 +1,13 @@
 package job
 
 // define the job struct here
+
+type Job struct {
+	JobID              string
+	JobType            string
+	Payload            map[string]interface{}
+	MaxRetry           int8
+	CurentAttemptCount int8
+	EnqueuedAt         int64
+	ScheduledAt        int64
+}
