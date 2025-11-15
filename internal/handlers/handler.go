1d68462c6dcdd0d60073d7dd4292f62fd8228543
pcodefromAO <primecoder8@gmail.com>
2025-11-15 14:31:03 +0530

    add handler and route

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -1,3 +1,10 @@
 package handler
 
-// this will contain all the job handlers
+// this will contain all the job handler
+
+func EnqueueHandler () {
+	// parse the request json 
+	// validate the required field
+	// create a new job instance
+	// call the broker enque it will return the jobid or error 
+}
